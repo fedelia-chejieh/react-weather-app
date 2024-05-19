787836aca7bbdf4dc720c3d021cbbac73d3ce330
Fedelia Chejieh <fedelia.cj@gmail.com>
2024-05-19 22:10:18 +0100

    Added next page directory to footer links

--- src/Footer.js
+++ src/Footer.js
@@ -5,12 +5,18 @@ export default function Footer() {
   return (
     <footer className="footer">
       This project was coded by{" "}
-      <a href="https://github.com/fedelia-chejieh">Fedelia Chejieh,</a> is{" "}
-      <a href="https://github.com/fedelia-chejieh/react-weather-app">
+      <a href="https://github.com/fedelia-chejieh" target="_blank">
+        Fedelia Chejieh,
+      </a>{" "}
+      is{" "}
+      <a
+        href="https://github.com/fedelia-chejieh/react-weather-app"
+        target="_blank"
+      >
         open-sourced on Github
       </a>{" "}
       and{" "}
-      <a href="https://elaborate-blini-e170c7.netlify.app/">
+      <a href="https://elaborate-blini-e170c7.netlify.app/" target="_blank">
         hosted on Netlify
       </a>
     </footer>
