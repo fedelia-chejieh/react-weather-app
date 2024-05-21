89ca956163ffce121e02745b35628ccab588a460
Fedelia Chejieh <fedelia.cj@gmail.com>
2024-05-21 12:11:37 +0100

    Added bootsrap components to search form

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <form>
-          <input
-            type="search"
-            placeholder="Enter a city.."
-            className="Search-input"
-          />
-          <button className="Search-button">Search</button>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="Search-input"
+              />
+            </div>
+            <div className="col-3">
+              <button className="Search-button">Search</button>
+            </div>
+          </div>
         </form>
       </header>
     </div>
