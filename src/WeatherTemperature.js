1440a663eacbe1e8c3cb191a15aec22df80369f3
Fedelia Chejieh <fedelia.cj@gmail.com>
2024-06-15 19:14:40 +0100

    removed Fahrenheit from displaying

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,44 +1,10 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celcius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelcius(event) {
-    event.preventDefault();
-    setUnit("celcius");
-  }
-  if (unit === "celcius") {
-    return (
-      <div className="WeatherTemperatue">
-        <div className="Weather-app-temperature">{props.celcius}</div>
-        <span className="Weather-app-unit">
-          <a href="/" onClick={showFahrenheit}>
-            °C{" "}
-          </a>
-          |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celcius * 9) / 5 + 32;
-    return (
-      <div className="WeatherTemperatue">
-        <div className="Weather-app-temperature">{Math.round(fahrenheit)}</div>
-        <span className="Weather-app-unit">
-          <a href="/" onClick={showCelcius}>
-            °C{" "}
-          </a>
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperatue">
+      <div className="Weather-app-temperature">{props.celcius}</div>
+      <span className="Weather-app-unit">°C </span>
+    </div>
+  );
 }
